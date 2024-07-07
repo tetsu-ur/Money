499d9c304fb2b6ecd4d36fbd9ad7b6276a43e0e7
tetsu-ur <t-hirata@leo.nifty.jp>
2024-07-07 21:16:49 +0900

    first commit

---
+++ src/test/money/Tttt.java
@@ -0,0 +1,14 @@
+package test.money;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Tttt {
+
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+}
